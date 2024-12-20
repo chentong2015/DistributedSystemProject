3638d29b332d893580ef45c4a8e07864e8e84514
tchen <chen2015tong@gmail.com>
2024-12-20 14:46:53 +0100

    update cache system app

--- caching-technology/cache-system-sample/src/main/java/org/example/controller/CachingController.java
+++ caching-technology/cache-system-sample/src/main/java/org/example/controller/CachingController.java
@@ -1,5 +1,8 @@
 package org.example.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.example.helper.SpringRedisHelper;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CachingController {
 
-    private final StringRedisTemplate stringRedisTemplate = SpringRedisHelper.getStringJedisTemplate();
-
     @GetMapping("/cache")
     public String getCacheValue() {
-        stringRedisTemplate.opsForValue().set("newKey", "newValue");
+        StringRedisTemplate redisTemplate = SpringRedisHelper.getStringJedisTemplate();
 
-        String value = stringRedisTemplate.opsForValue().get("key1");
+        // API for String value
+        redisTemplate.opsForValue().set("newKey", "newValue");
+        String value = redisTemplate.opsForValue().get("key1");
         System.out.println(value);
+
+        // API for List values
+        List<String> list = redisTemplate.boundListOps("myList").range(0, -1);
+        for (String item: list) {
+            System.out.println(item);
+        }
+
+        // API for HashMap values
+        Map<Object, Object> map = redisTemplate.boundHashOps("myHash").entries();
+        for (Map.Entry<Object, Object> entry: map.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
         return value;
     }
 }
